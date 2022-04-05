33d7bd3ca870199fbd379ee2bc9a5baff58175fc
Denis Bao <denis.tpd@gmail.com>
2022-04-05 15:04:53 -0300

    iniciando estilo e inputs do form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { ExpenseList } from './components/Expenses/ExpenseList'
+import { NewExpense } from './components/NewExpese/NewExpense'
 
 import { data_expenses } from './data/expenses'
 
 export function App() {
   return (
     <div>
-      <h1>Let's get started!</h1>
+      <NewExpense />
       <ExpenseList expenses={data_expenses}/>
     </div>
   )
---
+++ src/components/NewExpese/NewExpense.jsx
@@ -0,0 +1,11 @@
+import { ExpenseForm } from './ExpenseForm'
+
+import './NewExpense.css'
+
+export function NewExpense() {
+  return (
+    <div className='new-expense'>
+      <ExpenseForm />
+    </div>
+  )
+}
