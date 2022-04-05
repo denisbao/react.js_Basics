a532e98e2565b312b5c7e1d679a90b8c49439069
Denis Bao <denis.tpd@gmail.com>
2022-04-05 17:42:16 -0300

    iniciando a filtragem de dispesas

--- src/components/Expenses/ExpenseList.jsx
+++ src/components/Expenses/ExpenseList.jsx
@@ -1,20 +1,32 @@
 import { ExpenseItem } from "./ExpenseItem"
+import { ExpenseFilter } from './ExpenseFilter';
 import { Card } from '../UI/Card';
 
 import './ExpenseList.css'
+import { useState } from 'react';
 
 export function ExpenseList(props) {
+  const [filteredYear, setFilteredYear] = useState('2022')
+
+  function filterChangeHandler(selectedYear) {
+    setFilteredYear(selectedYear)
+  }
+
   return (
-    <Card className="expenses">
-      {props.expenses.map(expense => (
-        <div key={expense.id}>
-          <ExpenseItem
-            title={expense.title}
-            date={expense.date}
-            amount={expense.amount} 
-          />
-        </div>
-      ))}
-    </Card>
+    <>
+      <Card className="expenses">
+        <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        {props.expenses.map(expense => (
+          <div key={expense.id}>
+            <ExpenseItem
+              title={expense.title}
+              date={expense.date}
+              amount={expense.amount} 
+            />
+          </div>
+        ))}
+      </Card>
+    </>
+    
   )
 }
